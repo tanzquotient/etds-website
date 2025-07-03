84aee34643a302cdf984a12ec31a798f1057323e
Burkhard Ringlein <mail@burkhard.engineer>
2025-07-03 22:07:23 +0200

    trying to scale...

--- lost-found/lost-found.js
+++ lost-found/lost-found.js
@@ -13,9 +13,20 @@ function loadImg() {
     // - Fixed offset from the top of top of the page.
     // - Center cursor (default is top left)
     var offset = 170 + Math.max(400 - screen.width, 0)
-    document.getElementById("canvas").style = "top: " + offset + "px; position: absolute; left: 0px; width: 100%;"
-    const x_trans = parseInt(x) - 25;
-    const y_trans = parseInt(y) + offset - 25;
+    document.getElementById("canvas").style = "top: " + offset + "px; position: absolute; left: 0px;"
+    var x_trans = parseInt(x) - 25;
+    var y_trans = parseInt(y) + offset - 25;
+    if(urlParams.has("width"))
+    {
+        const orig_width = urlParams.get('width');
+        const orig_height = urlParams.get('height');
+        var scale_x = screen.width/parseInt(orig_width);
+        var scale_y = screen.height/parseInt(orig_height);
+        var x_trans = parseInt(x) * scale_x - 25;
+        var offset = 170 + Math.max(400 - parseInt(orig_width), 0)
+        var y_trans = parseInt(y) * scale_y + offset - 25;
+
+    }
 
     document.getElementById("img_id").value = img_id;
 
@@ -24,7 +35,7 @@ function loadImg() {
 
     document.getElementById("canvas").src = "/assets/img/lost-found/" + img_id + ".jpeg";
 
-    if (x != null) {
+    if (y != null) {
         claim_code = img_id + "_" + x + "_" + y;
         document.getElementById("claim_code").value = claim_code;
 
@@ -36,6 +47,6 @@ function loadImg() {
         //	 );
 
         // if we want to do it via google forms
-        document.getElementById("entry.1487141201").value = location.toString();
+        document.getElementById("entry.1487141201").value = location.toString() + "&width=" + screen.width + "&height=" + screen.height;
     }
 }
